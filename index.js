f05e041f4b9ab514095dde01b626883122ed8cd1
Jaqueline Silva <jhacck25@gmail.com>
2022-10-25 21:10:54 -0300

    subindo conteudo da aula

---
+++ index.js
@@ -0,0 +1,16 @@
+const funcionaria = { 
+    nome: "Jaqueline Silva", 
+    idade: 23, 
+    cargo: "Quality Assurance", 
+    cidade: "Taubaté"
+};
+console.log("A funcionária se chama " + 
+funcionaria.nome +
+ ", ela tem " + 
+ funcionaria.idade + 
+ " anos, mora em " + 
+ funcionaria.cidade +
+ " e atualmente trabalha como " + 
+ funcionaria.cargo + " na Embras.")
+
+ //Arrays são traduzidos com listas, vetores 
